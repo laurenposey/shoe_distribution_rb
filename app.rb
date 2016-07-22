91c329ca9e0386caf6ece9007c9253a5edbc86ef
Lauren Posey <mrslaurenposey@gmail.com>
2016-07-22 09:55:19 -0700

    finally got rake to work

--- app.rb
+++ app.rb
@@ -1,8 +1,9 @@
+require('pg')
 require('sinatra')
 require('sinatra/reloader')
 require("sinatra/activerecord")
 require('./lib/brand')
 require('./lib/store')
 also_reload('lib/**/*.rb')
-require('pg')
+
 require('pry')

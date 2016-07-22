9b90966cf4dc90386023e2fef1a5ad2d8b04c92c
Lauren Posey <mrslaurenposey@gmail.com>
2016-07-22 09:06:06 -0700

    first commit rake not working on my machine because incompatable encoding

---
+++ app.rb
@@ -0,0 +1,8 @@
+require('sinatra')
+require('sinatra/reloader')
+require("sinatra/activerecord")
+require('./lib/brand')
+require('./lib/store')
+also_reload('lib/**/*.rb')
+require('pg')
+require('pry')
---
+++ spec/spec_helper.rb
@@ -0,0 +1,16 @@
+ENV['RACK_ENV']='test'
+require("rspec")
+require("pg")
+require("sinatra/activerecord")
+require('brand')
+require('store')
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Brand.all().each() do |brand|
+      brand.destroy()
+    end
+    Store.all().each() do |store|
+      store.destroy()
+    end
+  end
